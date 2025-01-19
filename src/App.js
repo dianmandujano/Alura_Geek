d32a9814935aed4fd0e3e509f21c3d1136d6242d
DavidFPR <136135434+DavidFPR@users.noreply.github.com>
2025-01-19 15:27:35 -0600

    Arregla problemas con dependencias de React..

--- src/App.js
+++ src/App.js
@@ -1,93 +1,119 @@
-import React, { useState, useEffect } from "react";
-import { AppWrapper, ButtonWrapper } from "./App.styled";
-import Header from "./components/Header/Header";
-import Card from "./components/Card/Card";
-import CustomInput from "./components/CustomInput/CustomInput";
-import Button from "./components/Button/Button";
-import Footer from "./components/Footer/Footer";
-import { ProductSideWrapper, ProductFormSideWrapper, BodyWrapper, HeaderWrapper, ProductSideStore, FooterWrapper } from "./App.styled";
+import React, { useState, useEffect } from 'react';
+import { AppWrapper, ButtonWrapper } from './App.styled';
+import Header from './components/Header/Header';
+import Card from './components/Card/Card';
+import CustomInput from './components/CustomInput/CustomInput';
+import Button from './components/Button/Button';
+import Footer from './components/Footer/Footer';
+import {
+  ProductSideWrapper,
+  ProductFormSideWrapper,
+  BodyWrapper,
+  HeaderWrapper,
+  ProductSideStore,
+  FooterWrapper,
+} from './App.styled';
 
 const App = () => {
-  const [name, setName] = useState("");
-  const [price, setPrice] = useState("");
-  const [image, setImage] = useState("");
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+  const [image, setImage] = useState('');
 
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:3000/products")
+    fetch('http://localhost:3000/products')
       .then((response) => response.json())
       .then((data) => setProducts(data))
       .catch((error) => console.error(error));
   }, []);
 
   const handleProducts = () => {
-    fetch("http://localhost:3000/products", {
-      method: "POST",
+    fetch('http://localhost:3000/products', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({ name, price, image }),
     })
-    .then((response) => response.json())
-    .then((data) => setProducts([...products, data]))
-    .catch((error) => console.error(error));
+      .then((response) => response.json())
+      .then((data) => setProducts([...products, data]))
+      .catch((error) => console.error(error));
 
     clearFields();
-  }
+  };
 
   const handleDelete = (id) => {
     fetch(`http://localhost:3000/products/${id}`, {
-      method: "DELETE",
+      method: 'DELETE',
     })
-    .then((response) => response.json())
-    .then((data) => setProducts(products.filter((product) => product.id !== id)))
-    .catch((error) => console.error(error));
-  }
+      .then((response) => response.json())
+      .then((data) =>
+        setProducts(products.filter((product) => product.id !== id))
+      )
+      .catch((error) => console.error(error));
+  };
 
   const clearFields = () => {
-    setName("");
-    setPrice("");
-    setImage("");
-  }
+    setName('');
+    setPrice('');
+    setImage('');
+  };
 
   return (
     <AppWrapper>
-      {/* Header */}
-      <HeaderWrapper> 
+      <HeaderWrapper>
         <Header />
       </HeaderWrapper>
-      {/* Body */}
       <BodyWrapper>
         <ProductSideWrapper>
           <p>MIS PRODUCTOS:</p>
           <ProductSideStore>
-          {products.length > 0 ? (
-            products.map((product) => (
-              <Card key={product.id} product={product} handleDelete={handleDelete} />
-            ))
-          ) : (
-            <p>No hay productos</p>
-          )}
+            {products.length > 0 ? (
+              products.map((product) => (
+                <Card
+                  key={product.id}
+                  product={product}
+                  handleDelete={handleDelete}
+                />
+              ))
+            ) : (
+              <p>No hay productos</p>
+            )}
           </ProductSideStore>
         </ProductSideWrapper>
         <ProductFormSideWrapper>
           <p>AGREGAR PRODUCTO:</p>
-          <CustomInput placeholder="Nombre..." onChange={(e) => setName(e.target.value)} value={name} />
-          <CustomInput placeholder="Precio..." onChange={(e) => setPrice(e.target.value)} value={price} />
-          <CustomInput placeholder="Imagen..." onChange={(e) => setImage(e.target.value)} value={image} />
+          <CustomInput
+            placeholder="Nombre..."
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+          <CustomInput
+            placeholder="Precio..."
+            onChange={(e) => setPrice(e.target.value)}
+            value={price}
+          />
+          <CustomInput
+            placeholder="Imagen..."
+            onChange={(e) => setImage(e.target.value)}
+            value={image}
+          />
           <ButtonWrapper>
-          <Button placeholder="Agregar" onClick={handleProducts}>Agregar</Button>
-          <Button placeholder="Limpiar" onClick={clearFields}>Limpiar</Button>
+            <Button placeholder="Agregar" onClick={handleProducts}>
+              Agregar
+            </Button>
+            <Button placeholder="Limpiar" onClick={clearFields}>
+              Limpiar
+            </Button>
           </ButtonWrapper>
         </ProductFormSideWrapper>
       </BodyWrapper>
-      {/* Footer */}
       <FooterWrapper>
         <Footer />
       </FooterWrapper>
     </AppWrapper>
   );
-}
+};
 
 export default App;
